b59196e27403fb467ffef0caf3ed700072a81a10
jemian <prjemian@gmail.com>
2017-03-20 18:12:26 -0500

    fixes #3: generalize using a `settings.ini` file

--- src/bcdamenu/launcher.py
+++ src/bcdamenu/launcher.py
@@ -7,25 +7,19 @@ BcdaMenu: Creates a GUI menu button to start common beam line software
 import datetime
 import os
 import sys
-# TODO: import argparse
+import argparse
 from PyQt4.QtGui import *
 from functools import partial
 import subprocess
+from collections import OrderedDict
+try:
+    import configparser as iniParser
+except:
+    import ConfigParser as iniParser
 
 
-USAXS_CONFIG = {
-    'title': '9-ID-C USAXS menu',
-    'menu_items': {
-        "9-ID-C USAXS controls (MEDM)": "start_epics",
-        "separator": None,
-        "USAXS Q calculator": "qToolUsaxs.csh",
-        "sample and detector XY position tool": "wxmtxy.csh",
-        "USAXS sample stage tool": "/home/beams/USAXS/Apps/wxmtusaxs/wxmtusaxs",
-        "PyMca": "/APSshare/bin/pymca",
-        "SAXS Imaging tool": "/APSshare/epd/rh6-x86/bin/python /home/beams/USAXS/Apps/USAXS_dataworker/Main.py",
-        "Save Instr. status to Elog": "saveToElog.csh",
-    }
-}
+DEFAULT_SECTION_LABEL = 'DEFAULT'
+MENU_ITEMS_SECTION_LABEL = 'menu_items'
 
 
 class MainButtonWindow(QWidget):
@@ -33,12 +27,15 @@ class MainButtonWindow(QWidget):
 
     def __init__(self, parent=None, config=None):
         QWidget.__init__(self, parent)
-        self.config = config or USAXS_CONFIG
+        self.config = config or {}
 
         self.popup  = QPushButton('Commands...')
         self.menu   = QMenu()
         self.popup.setMenu(self.menu)
-        for k, v in self.config.get('menu_items', {}).items():
+        
+        # fallback to empty dictionary if not found
+        config_dict = self.config.get(MENU_ITEMS_SECTION_LABEL, {})
+        for k, v in config_dict.items():
             if k == 'separator' and v is None:
                 self.menu.addSeparator()
             else:
@@ -59,30 +56,73 @@ class MainButtonWindow(QWidget):
     
     def receiver(self, label, command):
         '''handle commands from menu button'''
-        command = os.path.normpath(command)
         msg = 'BcdaMenu (' 
         msg += str(datetime.datetime.now())
         msg += '), ' + label
-        msg += ':  ' + command
+        if command is None:
+            msg += ': '
+        else:
+            command = os.path.normpath(command)
+        msg += ':  ' + str(command)
         print(msg)
-        subprocess.Popen(command, shell = True)
+        if command is not None:
+            subprocess.Popen(command, shell = True)
     
     def about_box(self):
         '''TODO: should display an About box'''
         print(__doc__)
 
 
-def gui():
+def gui(config = None):
     '''display the main widget'''
     app = QApplication(sys.argv)
-    probe = MainButtonWindow()
+    probe = MainButtonWindow(config=config)
     probe.show()
     sys.exit(app.exec_())
 
 
 def main():
     '''process any command line options before starting the GUI'''
-    gui()
+    doc = __doc__.strip().splitlines()[0]
+    #doc += '\n  v' + __init__.__version__
+    parser = argparse.ArgumentParser(prog='BcdaMenu', description=doc)
+    parser.add_argument('settingsfile', help="Settings file (.ini)")
+    params = parser.parse_args()
+
+    if not os.path.exists(params.settingsfile):
+        raise IOError('file not found: ' + params.settingsfile)
+
+    config = iniParser.ConfigParser(allow_no_value=True)
+    config.read(params.settingsfile)
+    
+    settings = dict(title='BcdaMenu', menu_items=OrderedDict())
+    for k, v in config.items(DEFAULT_SECTION_LABEL):
+        settings[k] = v
+
+    # parse the settings file and coordinate numbered labels with commands
+    labels = {}
+    commands = {}
+    menu_items_dict = dict(config.items(MENU_ITEMS_SECTION_LABEL))
+    for k, v in menu_items_dict.items():
+        if k == 'title': continue
+        parts = k.split()
+        if parts[0] not in ('command', 'label'):
+            msg = 'Error in settings file, section [menu_items]: ' + params.settingsfile
+            msg += '\n  line reading: ' + k + ' = ' + v
+            raise KeyError(msg)
+        item = 'key_%04d' % int(parts[1])
+        if parts[0] == 'label':
+            labels[parts[1]] = v
+        elif parts[0] == 'command':
+            if v == 'None':
+                v = None
+            commands[parts[1]] = v
+    
+    # add the menu items in numerical order
+    for k, label in sorted(labels.items()):
+        settings[MENU_ITEMS_SECTION_LABEL][label] = commands[k]
+
+    gui(config = settings)
 
 
 if __name__ == '''__main__''':
---
+++ src/bcdamenu/make_ini.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+'''
+Create a settings.ini file from a dictionary
+'''
+
+try:
+    import configparser as iniParser
+except:
+    import ConfigParser as iniParser
+import launcher
+
+
+INI_FILE = 'settings.ini'
+USAXS_CONFIG = {
+    'title': '9-ID-C USAXS menu',
+    'menu_items': {
+        "9-ID-C USAXS controls (MEDM)": "start_epics",
+        "separator": None,
+        "USAXS Q calculator": "qToolUsaxs.csh",
+        "sample and detector XY position tool": "wxmtxy.csh",
+        "USAXS sample stage tool": "/home/beams/USAXS/Apps/wxmtusaxs/wxmtusaxs",
+        "PyMca": "/APSshare/bin/pymca",
+        "SAXS Imaging tool": "/APSshare/epd/rh6-x86/bin/python /home/beams/USAXS/Apps/USAXS_dataworker/Main.py",
+        "Save Instr. status to Elog": "saveToElog.csh",
+    }
+}
+
+config = iniParser.ConfigParser()
+#config.read(INI_FILE)
+config.set(launcher.DEFAULT_SECTION_LABEL, 'title', USAXS_CONFIG['title'])
+config.add_section(launcher.MENU_ITEMS_SECTION_LABEL)
+for index, k in enumerate(USAXS_CONFIG[launcher.MENU_ITEMS_SECTION_LABEL].keys()):
+    v = USAXS_CONFIG[launcher.MENU_ITEMS_SECTION_LABEL][k]
+    config.set(launcher.MENU_ITEMS_SECTION_LABEL, 'label %d' % (index+1), k)
+    config.set(launcher.MENU_ITEMS_SECTION_LABEL, 'command %d' % (index+1), v)
+with open(INI_FILE, 'w') as configfile:    # save
+    config.write(configfile)
